0df93ecc49f38f4b7d2dfcdc7452969377a463aa
ccumrine26 <124619220+ccumrine26@users.noreply.github.com>
2023-02-10 11:49:57 -0700

    Create double_led_blink.ino

---
+++ double_led_blink.ino
@@ -0,0 +1,51 @@
+void setup() {
+  pinMode(5, OUTPUT);
+  digitalWrite(5, HIGH);
+  delay(200);
+  digitalWrite(5, LOW);
+  delay(1000);
+  pinMode(5, OUTPUT);
+  digitalWrite(5, HIGH);
+  delay(200);
+  digitalWrite(5, LOW);
+  delay(1000);
+  pinMode(6, OUTPUT);
+  digitalWrite(6, HIGH);
+  delay(200);
+  digitalWrite(6, LOW);
+  delay(1000);
+  pinMode(6, OUTPUT);
+  digitalWrite(6, HIGH);
+  delay(200);
+  digitalWrite(6, LOW);
+  delay(1000);
+  pinMode(6, OUTPUT);
+  digitalWrite(6, HIGH);
+  delay(200);
+  digitalWrite(6, LOW);
+  delay(1000);
+  pinMode(6, OUTPUT);
+  digitalWrite(6, HIGH);
+  delay(200);
+  digitalWrite(6, LOW);
+  pinMode(5, OUTPUT);
+  digitalWrite(5, HIGH);
+  delay(200);
+  digitalWrite(5, LOW);
+  delay(1000);
+   pinMode(6, OUTPUT);
+  digitalWrite(6, HIGH);
+  delay(200);
+  digitalWrite(6, LOW);
+}
+
+void loop() {
+    digitalWrite(5, HIGH);
+    delay(200);
+    digitalWrite(5, LOW);
+    delay(200);
+    digitalWrite(6, HIGH);
+    delay(200);
+    digitalWrite(6, LOW);
+    delay(200);
+}
